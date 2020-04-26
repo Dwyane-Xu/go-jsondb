b7d22c773690e43c7d2f67f1e94e0485009b1beb
xujinzhao <502794297@qq.com>
2020-04-27 00:17:12 +0800

    change the project structure

---
+++ example/fishing.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"go-jsondb"
+)
+
+type Fish struct {
+	Name   string  `json:"name"`
+	Old    uint    `json:"old"`
+	Weight float32 `json:"weight"`
+}
+
+func main() {
+	db, err := jsondb.New("/Users/xujinzhao/许锦钊/程序/Go/go-jsondb/json-file", nil)
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+
+	fish := Fish{"fish man", 3, 11.2}
+	if err := db.Write("fish", "onefish", fish); err != nil {
+		fmt.Println("Error", err)
+	}
+
+	var readFish Fish
+	if err := db.Read("fish", "onefish", &readFish); err != nil {
+		fmt.Println("Error", err)
+	}
+	fmt.Println(readFish)
+
+	var fishies []Fish
+	records, err := db.ReadAll("fish")
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+
+	for _, f := range records {
+		fishFound := Fish{}
+		if err := json.Unmarshal([]byte(f), &fishFound); err != nil {
+			fmt.Println("Error", err)
+		}
+		fishies = append(fishies, fishFound)
+	}
+	fmt.Println(fishies)
+
+	if err := db.Delete("fish", "onefish"); err != nil {
+		fmt.Println("Error", err)
+	}
+
+	if err := db.Delete("fish", ""); err != nil {
+		fmt.Println("Error", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-jsondb
+
+go 1.13
+
+require github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25
